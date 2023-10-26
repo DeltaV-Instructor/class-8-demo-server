404ae86df5f6ff9f1ed119a51ae66f425a4ed67e
tektechnologies <info@tektechnologies.com>
2023-10-26 13:47:31 -0500

    add old code

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 'use strict';
-console.log('image finder server is connected!!!');
-//REQUIRES
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const getPhotos = require('./modules/photo.js');
 
-//USE
+require('dotenv');
+const express = require('express');
+// const cors = require('cors');
+
+const weather = require('./modules/weather.js');
 const app = express();
-app.use(cors());
+console.log(process.env);
 const PORT = process.env.PORT || 3002;
 
-//ROUTES
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the Image Finder Server, have a great day!');
-});
-app.get('/photos', getPhotos);
+app.get('/weather', weatherHandler);
 
-app.get('*', (req, res) => {
-  res.status(404).send('These are not the droids your looking 404.');
-});
 
-//LISTEN
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send('Sorry. Something went wrong!');
+    });
+}
 
+app.listen(PORT, () => console.log(`Server up on ${PORT}`));
